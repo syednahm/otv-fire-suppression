cb0e85aa5eb6183f9be0ccd412775793e46b4ea4
syednahm <syednaz2006@gmail.com>
2026-03-23 15:28:27 -0400

    Hello World

--- src/main.cpp
+++ src/main.cpp
@@ -6,10 +6,12 @@ int myFunction(int, int);
 void setup() {
   // put your setup code here, to run once:
   int result = myFunction(2, 3);
+  Serial.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  Serial.println("Hello, World!");
 }
 
 // put function definitions here:

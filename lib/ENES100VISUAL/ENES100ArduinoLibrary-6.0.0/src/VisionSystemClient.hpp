a5022fd18e862b6caeb8df645f066234d2ef434e
annab <abanzuk1@terpmail.umd.edu>
2026-03-25 15:43:55 -0400

    added ENES100ArduinoLibrary-6.0.0 to lib, updated main.cpp to include ENES library

---
+++ lib/ENES100VISUAL/ENES100ArduinoLibrary-6.0.0/src/VisionSystemClient.hpp
@@ -0,0 +1,143 @@
+#ifndef VisionSystemClient_hpp
+#define VisionSystemClient_hpp
+
+#include "Arduino.h"
+#include "SoftwareSerial.h"
+
+// Transmission opcodes
+#define OP_BEGIN            0x1
+#define OP_PRINT            0x2
+#define OP_CHECK            0x3
+#define OP_MISSION          0x4
+#define OP_ML_PREDICTION    0x5
+#define OP_ML_CAPTURE       0x6
+#define OP_IS_CONNECTED     0x7
+#define OP_DEBUG_STATUS     0x8
+
+const byte FLUSH_SEQUENCE[] = {0xFF, 0xFE, 0xFD, 0xFC};
+
+class Coordinate {
+public:
+    Coordinate();
+    Coordinate(double x, double y);
+    Coordinate(double x, double y, double theta);
+
+    double x;
+    double y;
+    double theta;
+};
+
+struct Enes100DebugStatus {
+    bool valid = false;
+
+    bool wifiConnected = false;
+    bool wsStarted = false;
+    bool wsConnected = false;
+    bool hasBegin = false;
+    bool everWsConnected = false;
+    bool lastConnectCallOk = false;
+    bool routeIsFallback = false;
+    bool wifiJoinInProgress = false;
+
+    uint8_t wifiStatus = 0;
+    uint16_t room = 0;
+    uint8_t routeIndex = 0;
+    uint8_t routeCount = 0;
+    uint16_t connectAttempts = 0;
+    uint8_t openEvents = 0;
+    uint8_t closeEvents = 0;
+    uint8_t lastEvent = 0;
+    int8_t rssi = 0;
+
+    uint8_t localIp[4] = {0, 0, 0, 0};
+    uint8_t lastDisconnectReason = 0;
+    uint16_t wifiBeginCount = 0;
+    uint16_t wifiGotIpCount = 0;
+    uint8_t wifiJoinAgeSec = 0;
+
+    char currentUrl[64] = {0};
+};
+
+class VisionSystemClient {
+public:
+    bool isConnected();
+    byte state();
+    void begin(const char* teamName, byte teamType, int markerId, int roomNumber, int wifiModuleTX, int wifiModuleRX);
+
+    float getX();
+    float getY();
+    float getTheta();
+    bool isVisible();
+    bool updateLocation();
+
+    void mission(int type, int message);
+    void mission(int type, double message);
+    void mission(int type, char message);
+    void mission(int type, Coordinate message);
+    int MLGetPrediction(int);
+    void MLCaptureTrainingImage(const char * label);
+
+    bool debugStatus(Enes100DebugStatus& out);
+    void debugDump(Stream& out);
+
+    template <typename T>
+    void print(T message);
+    template <typename T>
+    void println(T message);
+
+    Coordinate location;
+
+private:
+    bool receive(Coordinate* coordinate = NULL);
+    bool visible = false;
+    void updateIfNeeded();
+    void readBytes(byte* buffer, int length);
+
+    void clearInput(uint16_t quietMs = 4, uint16_t maxMs = 40);
+    byte queryState(uint16_t timeoutMs = 30);
+    void sendBeginPacket();
+    void maintainConnection(bool aggressive = false);
+
+    bool readByteTimeout(byte& out, uint16_t timeoutMs);
+    bool readExact(byte* buffer, int length, uint16_t timeoutMs);
+
+    uint32_t lastUpdate = 0;
+    uint32_t mLastBeginSendMs = 0;
+    uint32_t mLastStatePollMs = 0;
+    byte mLastState = 0xFF;
+
+    int mMarkerId = 0;
+    int mRoomNumber = 0;
+    byte mTeamType = 0;
+    char mTeamName[64] = {0};
+    bool mConfigValid = false;
+
+    SoftwareSerial* mSerial = nullptr;
+};
+
+template <typename T>
+void VisionSystemClient::print(T message) {
+    if (mSerial == nullptr) return;
+    maintainConnection();
+
+    mSerial->write(OP_PRINT);
+    mSerial->print(message);
+    mSerial->write((byte)0x00);
+    mSerial->write(FLUSH_SEQUENCE, 4);
+    mSerial->flush();
+}
+
+template <typename T>
+void VisionSystemClient::println(T message) {
+    if (mSerial == nullptr) return;
+    maintainConnection();
+
+    mSerial->write(OP_PRINT);
+    mSerial->print(message);
+    mSerial->write('\n');
+    mSerial->write((byte)0x00);
+    mSerial->write(FLUSH_SEQUENCE, 4);
+    mSerial->flush();
+}
+
+#endif /* VisionSystemClient_hpp */
--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <Arduino.h>
+#include <Enes100.h>
 
 // put pin definitions here:
 const int left_switch = 2;
@@ -33,6 +34,9 @@ void turnLeft(int speed, int duration);
 void turnRight(int speed, int duration);
 
 void setup() {
+    Enes100.begin("Phoenix ", FIRE, markerId, 1120, 11, 12);
+    // At this point we know we are connected.
+    Enes100.println("Connected...");
   // put your setup code here, to run once:
   int result = myFunction(2, 3);
   Serial.begin(9600);
@@ -40,7 +44,34 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  Serial.println("Hello, World!");
+  
+   float x, y, t; bool v; // Declare variables to hold the data
+    //Enes100.getX will make sure you get the latest data available to you about your OTV's location.
+    //The first time getX is called, X, Y, theta and visibility are queried and cached.
+    //Subsequent calls return from the cache, so there is no performance gain to saving the function response to a variable.
+
+    x = Enes100.getX();  // Your X coordinate! 0-4, in meters, -1 if no aruco is not visibility (but you should use Enes100.isVisible to check that instead)
+    y = Enes100.getY();  // Your Y coordinate! 0-2, in meters, also -1 if your aruco is not visible.
+    t = Enes100.getTheta();  //Your theta! -pi to +pi, in radians, -1 if your aruco is not visible.
+    v = Enes100.isVisible(); // Is your aruco visible? True or False.
+
+    if (v) // If the ArUco marker is visible
+    {
+        Enes100.print(x); // print out the location
+        Enes100.print(",");
+        Enes100.print(y);
+        Enes100.print(",");
+        Enes100.println(t);
+    }
+    else { // otherwise
+        Enes100.println("Not visible"); // print not visible
+    }
+
+    // Transmit the number of candles that are lit
+    Enes100.mission(NUM_CANDLES, 4);
+    // Transmit the topography of the fire mission (TOP_A for topography A)
+    Enes100.mission(TOPOGRAPHY, TOP_A);
+    delay(1000);
 }
 
 // put function definitions here:

327ba56b58d1be609c6b0e3b46b2f3e1a6a4d844
syednahm <syednaz2006@gmail.com>
2026-03-25 14:20:52 -0400

    Added pin numbers and variable declarations.

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,26 @@
 #include <Arduino.h>
 
+// put pin definitions here:
+const int left_switch = 2;
+const int right_switch = 4;
+const int left_motor_forward = 3;
+const int left_motor_backward = 5;
+const int right_motor_forward = 6;
+const int right_motor_backward = 9;
+const int dist_sensor_trigs = 7;
+const int dist_sensor_left_echo = 8;
+const int dist_sensor_right_echo = 13;
+const int fans = 10;
+const int wifiModuleTX = 11;
+const int wifiModuleRX = 12;
+
+byte teamType = FIRE;
+const char* teamName = "Phoenix";
+// replace with Aruco Marker ID.
+int markerId;
+int roomNumber = 1120;
+
+
 // put function declarations here:
 int myFunction(int, int);
 
